6073b4a55628dd23ce4ec6715394b52eaaea59fc
Vincent den Boer <vincent@shishkabab.net>
2019-02-11 18:14:08 +0100

    Refactor

--- ts/reconciliation.ts
+++ ts/reconciliation.ts
@@ -1,4 +1,4 @@
-import { ClientSyncLogEntry } from "./sync-log/types"
+import { ClientSyncLogEntry, ClientSyncLogCreationEntry, ClientSyncLogDeletionEntry, ClientSyncLogModificationEntry } from "./sync-log/types"
 
 export interface ExecutableOperation {
     operation : string
@@ -6,13 +6,16 @@ export interface ExecutableOperation {
     args : any[]
 }
 
-type Modifications = {[collection : string] : {[pk : string] : {
+type Modifications = {[collection : string] : CollectionModifications}
+type CollectionModifications = {[pk : string] : ObjectModifications}
+interface ObjectModifications {
     shouldBeCreated : boolean
     createdOn? : number
     isDeleted : boolean
     shouldBeDeleted : boolean
-    fields : {[field : string] : {createdOn : number, syncedOn? : number, value : any}}
-}}}
+    fields : {[field : string] : FieldModification}
+}
+type FieldModification = {createdOn : number, syncedOn? : number, value : any}
 
 function _throwModificationBeforeCreation(logEntry : ClientSyncLogEntry) {
     throw new Error(
@@ -28,60 +31,11 @@ export function reconcileSyncLog(logEntries : ClientSyncLogEntry[]) : Executable
         const pkAsJson = JSON.stringify(logEntry.pk)
         const objectModifications = collectionModifications[pkAsJson]
         if (logEntry.operation === 'modify') {
-            const updates = {createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn, value: logEntry.value}
-            if (!objectModifications) {
-                collectionModifications[pkAsJson] = {
-                    shouldBeCreated: false,
-                    isDeleted: !!logEntry.syncedOn,
-                    shouldBeDeleted: false,
-                    fields: {[logEntry.field]: updates}
-                }
-                continue
-            }
-            if (objectModifications.shouldBeCreated && objectModifications.createdOn > logEntry.createdOn) {
-                _throwModificationBeforeCreation(logEntry)
-            }
-            
-            const fieldModifications = objectModifications.fields[logEntry.field]
-            if (!fieldModifications) {
-                objectModifications[logEntry.field] = updates
-            } else if (logEntry.createdOn > fieldModifications.createdOn) {
-                Object.assign(fieldModifications, updates)
-            }
+            _processModificationEntry({objectModifications, logEntry, collectionModifications, pkAsJson})
         } else if (logEntry.operation === 'delete') {
-            const updates = {isDeleted: !!logEntry.syncedOn, shouldBeDeleted: true, fields: {}}
-            if (!objectModifications) {
-                collectionModifications[pkAsJson] = {shouldBeCreated: false, ...updates}
-            } else (
-                Object.assign(objectModifications, updates)
-            )
+            _processDeletionEntry({objectModifications, logEntry, collectionModifications, pkAsJson})
         } else if (logEntry.operation === 'create') {
-            if (!objectModifications) {
-                const fields = {}
-                for (const [key, value] of Object.entries(logEntry.value)) {
-                    fields[key] = {value, createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn}
-                }
-                collectionModifications[pkAsJson] = {
-                    shouldBeCreated: true, createdOn: logEntry.createdOn,
-                    isDeleted: false, shouldBeDeleted: false,
-                    fields
-                }
-            } else {
-                if (objectModifications.shouldBeCreated) {
-                    throw new Error(`Detected double create in collection '${logEntry.collection}', pk '${JSON.stringify(logEntry.pk)}'`)
-                }
-
-                const fields = objectModifications.fields
-                for (const [key, value] of Object.entries(logEntry.value)) {
-                    if (!fields[key]) {
-                        fields[key] = {value, createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn}
-                    } else if (logEntry.createdOn > fields[key].createdOn) {
-                        _throwModificationBeforeCreation(logEntry)
-                    }
-                }
-                objectModifications.shouldBeCreated = true
-                objectModifications.createdOn = logEntry.createdOn
-            }
+            _processCreationEntry({objectModifications, logEntry, collectionModifications, pkAsJson})
         }
     }
 
@@ -89,24 +43,106 @@ export function reconcileSyncLog(logEntries : ClientSyncLogEntry[]) : Executable
     for (const [collection, collectionModifications] of Object.entries(modificationsByObject)) {
         for (const [pkAsJson, objectModifications] of Object.entries(collectionModifications)) {
             const pk = JSON.parse(pkAsJson)
-            if (objectModifications.shouldBeDeleted) {
-                if (!objectModifications.isDeleted && !objectModifications.shouldBeCreated) {
-                    operations.push({operation: 'deleteOneObject', collection, args: [{pk}]})
-                }
-            } else if (objectModifications.shouldBeCreated) {
-                const object = {}
-                for (const [key, fieldModification] of Object.entries(objectModifications.fields)) {
-                    object[key] = fieldModification.value
-                }
-                operations.push({operation: 'createObject', collection, args: [object]})
-            } else {
-                for (const [fieldName, fieldModification] of Object.entries(objectModifications.fields)) {
-                    if (!fieldModification.syncedOn) {
-                        operations.push({operation: 'updateOneObject', collection, args: [{pk}, {[fieldName]: fieldModification.value}]})
-                    }
-                }
-            }
+            operations.push(...(_processModifications({objectModifications, collection, pk}) || []))
         }
     }
     return operations
+}
+
+export function _processCreationEntry(
+    {objectModifications, logEntry, collectionModifications, pkAsJson} :
+    {objectModifications : ObjectModifications, logEntry : ClientSyncLogCreationEntry,
+     collectionModifications : CollectionModifications, pkAsJson : any}
+) {
+    if (!objectModifications) {
+        const fields = {}
+        for (const [key, value] of Object.entries(logEntry.value)) {
+            fields[key] = {value, createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn}
+        }
+        collectionModifications[pkAsJson] = {
+            shouldBeCreated: true, createdOn: logEntry.createdOn,
+            isDeleted: false, shouldBeDeleted: false,
+            fields
+        }
+    } else {
+        if (objectModifications.shouldBeCreated) {
+            throw new Error(`Detected double create in collection '${logEntry.collection}', pk '${JSON.stringify(logEntry.pk)}'`)
+        }
+
+        const fields = objectModifications.fields
+        for (const [key, value] of Object.entries(logEntry.value)) {
+            if (!fields[key]) {
+                fields[key] = {value, createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn}
+            } else if (logEntry.createdOn > fields[key].createdOn) {
+                _throwModificationBeforeCreation(logEntry)
+            }
+        }
+        objectModifications.shouldBeCreated = true
+        objectModifications.createdOn = logEntry.createdOn
+    }
+}
+
+export function _processDeletionEntry(
+    {objectModifications, logEntry, collectionModifications, pkAsJson} :
+    {objectModifications : ObjectModifications, logEntry : ClientSyncLogDeletionEntry,
+     collectionModifications : CollectionModifications, pkAsJson : any}
+) {
+    const updates = {isDeleted: !!logEntry.syncedOn, shouldBeDeleted: true, fields: {}}
+    if (!objectModifications) {
+        collectionModifications[pkAsJson] = {shouldBeCreated: false, ...updates}
+    } else (
+        Object.assign(objectModifications, updates)
+    )
+}
+
+export function _processModificationEntry(
+    {objectModifications, logEntry, collectionModifications, pkAsJson} :
+    {objectModifications : ObjectModifications, logEntry : ClientSyncLogModificationEntry,
+     collectionModifications : CollectionModifications, pkAsJson : any}
+) {
+    const updates = {createdOn: logEntry.createdOn, syncedOn: logEntry.syncedOn, value: logEntry.value}
+    if (!objectModifications) {
+        collectionModifications[pkAsJson] = {
+            shouldBeCreated: false,
+            isDeleted: !!logEntry.syncedOn,
+            shouldBeDeleted: false,
+            fields: {[logEntry.field]: updates}
+        }
+        return
+    }
+    if (objectModifications.shouldBeCreated && objectModifications.createdOn > logEntry.createdOn) {
+        _throwModificationBeforeCreation(logEntry)
+    }
+    
+    const fieldModifications = objectModifications.fields[logEntry.field]
+    if (!fieldModifications) {
+        objectModifications[logEntry.field] = updates
+    } else if (logEntry.createdOn > fieldModifications.createdOn) {
+        Object.assign(fieldModifications, updates)
+    }
+}
+
+export function _processModifications(
+    {objectModifications, collection, pk} :
+    {objectModifications : ObjectModifications, collection : string, pk : any}
+) {
+    if (objectModifications.shouldBeDeleted) {
+        if (!objectModifications.isDeleted && !objectModifications.shouldBeCreated) {
+            return [{operation: 'deleteOneObject', collection, args: [{pk}]}]
+        }
+    } else if (objectModifications.shouldBeCreated) {
+        const object = {}
+        for (const [key, fieldModification] of Object.entries(objectModifications.fields)) {
+            object[key] = fieldModification.value
+        }
+        return [{operation: 'createObject', collection, args: [object]}]
+    } else {
+        const operations = []
+        for (const [fieldName, fieldModification] of Object.entries(objectModifications.fields)) {
+            if (!fieldModification.syncedOn) {
+                operations.push({operation: 'updateOneObject', collection, args: [{pk}, {[fieldName]: fieldModification.value}]})
+            }
+        }
+        return operations
+    }
 }
