1df9159a85608d29e0823e12338b41771712def9
Vincent den Boer <vincent@shishkabab.net>
2019-02-19 12:00:02 +0100

    Add PK utils

---
+++ ts/utils.test.ts
@@ -0,0 +1,73 @@
+import * as expect from 'expect'
+import StorageManager, { CollectionDefinitionMap } from '@worldbrain/storex';
+import { getObjectPk, getObjectWithoutPk } from './utils';
+
+describe('Primary key utils', () => {
+    async function setupTest(config : {collections : CollectionDefinitionMap}) {
+        const backend = {
+            configure: () => null,
+            operation: async (...args) => ({args})
+        } as any
+        const storageManager = new StorageManager({backend})
+        storageManager.registry.registerCollections(config.collections)
+        return { storageManager }
+    }
+
+    describe('getObjectPk()', () => {
+        it('should work for an object with a single field pk', async () => {
+            const { storageManager } = await setupTest({collections: {
+                user: {
+                    version: new Date('2019-02-19'),
+                    fields: {
+                        displayName: {type: 'string'}
+                    }
+                }
+            }})
+            expect(getObjectPk({id: 1, displayName: 'Joe'}, 'user', storageManager.registry)).toEqual(1)
+        })
+
+        it('should work for an object with a compound pk', async () => {
+            const { storageManager } = await setupTest({collections: {
+                user: {
+                    version: new Date('2019-02-19'),
+                    fields: {
+                        firstName: {type: 'string'},
+                        lastName: {type: 'string'},
+                        email: {type: 'string'}
+                    },
+                    pkIndex: ['firstName', 'lastName']
+                }
+            }})
+            expect(getObjectPk({firstName: 'Joe', lastName: 'Doe', email: 'bla@bla.com'}, 'user', storageManager.registry)).toEqual(['Joe', 'Doe'])
+        })
+    })
+
+    describe('getObjectWithoutPk()', () => {
+        it('should work for an object with a single field pk', async () => {
+            const { storageManager } = await setupTest({collections: {
+                user: {
+                    version: new Date('2019-02-19'),
+                    fields: {
+                        displayName: {type: 'string'}
+                    }
+                }
+            }})
+            expect(getObjectWithoutPk({id: 1, displayName: 'Joe'}, 'user', storageManager.registry)).toEqual({displayName: 'Joe'})
+        })
+
+        it('should work for an object with a compound pk', async () => {
+            const { storageManager } = await setupTest({collections: {
+                user: {
+                    version: new Date('2019-02-19'),
+                    fields: {
+                        firstName: {type: 'string'},
+                        lastName: {type: 'string'},
+                        email: {type: 'string'}
+                    },
+                    pkIndex: ['firstName', 'lastName']
+                }
+            }})
+            expect(getObjectWithoutPk({firstName: 'Joe', lastName: 'Doe', email: 'bla@bla.com'}, 'user', storageManager.registry)).toEqual({email: 'bla@bla.com'})
+        })
+    })
+})
---
+++ ts/utils.ts
@@ -0,0 +1,37 @@
+import { StorageRegistry } from "@worldbrain/storex";
+
+export function getObjectPk(object, collection : string, registry : StorageRegistry) {
+    const pkIndex = registry.collections[collection].pkIndex
+    if (typeof pkIndex === 'string') {
+        return object[pkIndex]
+    }
+
+    const pk = []
+    for (const indexField of pkIndex) {
+        if (typeof indexField === 'string') {
+            pk.push(object[indexField])
+        } else {
+            throw new Error(`getObject() called with relationship as pk, which is not supported yet.`)
+        }
+    }
+    return pk
+}
+
+export function getObjectWithoutPk(object, collection : string, registry : StorageRegistry) {
+    object = { ...object }
+
+    const pkIndex = registry.collections[collection].pkIndex
+    if (typeof pkIndex === 'string') {
+        delete object[pkIndex]
+        return object
+    }
+
+    for (const indexField of pkIndex) {
+        if (typeof indexField === 'string') {
+            delete object[indexField]
+        } else {
+            throw new Error(`getObject() called with relationship as pk, which is not supported yet.`)
+        }
+    }
+    return object
+}
