661a0fc0325a19be732329d4c4f1c1dd7f5251d1
Vincent den Boer <vincent@shishkabab.net>
2019-02-20 11:58:47 +0100

    Modularize sync logging middleware

--- ts/logging-middleware/index.ts
+++ ts/logging-middleware/index.ts
@@ -1,15 +1,13 @@
 import { StorageMiddleware } from '@worldbrain/storex/lib/types/middleware'
-import { ClientSyncLogStorage } from "../client-sync-log";
-import StorageManager, { OperationBatch } from '@worldbrain/storex';
-import { getObjectWithoutPk, getObjectPk } from '../utils';
-import { ClientSyncLogEntry } from '../client-sync-log/types';
-
-export type ExecuteAndLog = (originalOperation, logEntries : ClientSyncLogEntry[]) => Promise<any>
-export type OperationProcessorArgs = {next : {process: ({operation}) => any}, operation : any[], executeAndLog : ExecuteAndLog}
+import { ClientSyncLogStorage } from "../client-sync-log"
+import StorageManager from '@worldbrain/storex'
+import { ClientSyncLogEntry } from '../client-sync-log/types'
+import { OperationProcessorMap, DEFAULT_OPERATION_PROCESSORS } from './operation-processors'
 
 export class SyncLoggingMiddleware implements StorageMiddleware {
     private _clientSyncLog : ClientSyncLogStorage
     private _storageManager : StorageManager
+    private _operationProcessors : OperationProcessorMap = DEFAULT_OPERATION_PROCESSORS
 
     constructor({clientSyncLog, storageManager} : {clientSyncLog : ClientSyncLogStorage, storageManager : StorageManager}) {
         this._clientSyncLog = clientSyncLog
@@ -34,81 +32,15 @@ export class SyncLoggingMiddleware implements StorageMiddleware {
         }
 
         const operationType = (operation[0] as string)
-        if (operationType === 'createObject') {
-            return this._processCreateObject({next, operation, executeAndLog})
-        } else if (operationType === 'updateObject') {
-            return this._processUpdateObject({next, operation, executeAndLog})
-        } else if (operationType === 'updateObjects') {
-            return this._processUpdateObjects({next, operation, executeAndLog})
+        const operationProcessor = this._operationProcessors[operationType]
+        if (operationProcessor) {
+            return operationProcessor({next, operation, executeAndLog, getNow: () => this._getNow(), storageRegistry: this._storageManager.registry})
+        } else {
+            return next.process({operation})
         }
-
-        return next.process({operation})
-    }
-
-    async _processCreateObject({operation, executeAndLog} : OperationProcessorArgs) {
-        const [collection, value] = operation.slice(1)
-        const result = await executeAndLog(
-            {placeholder: 'object', operation: 'createObject', collection, args: value},
-            [{
-                collection,
-                createdOn: this._getNow(),
-                syncedOn: null,
-                operation: 'create',
-                pk: getObjectPk(value, collection, this._storageManager.registry),
-                value: getObjectWithoutPk(value, collection, this._storageManager.registry)
-            } as ClientSyncLogEntry]
-        )
-        const object = result.info.object.object
-        return {object}
     }
 
     _getNow() {
         return Date.now()
-    }
-
-    async _processUpdateObject({next, operation, executeAndLog} : OperationProcessorArgs) {
-        const [collection, where, updates] = operation.slice(1)
-        const pk = getObjectPk(where, collection, this._storageManager.registry);
-        const logEntries : ClientSyncLogEntry[] = []
-        for (const [fieldName, newValue] of Object.entries(updates)) {
-            logEntries.push({
-                createdOn: this._getNow(),
-                syncedOn: null,
-                field: fieldName,
-                collection,
-                operation: 'modify',
-                pk: pk,
-                value: newValue
-            })
-        }
-        await executeAndLog(
-            {placeholder: 'object', operation: 'updateObjects', collection, where, updates},
-            logEntries,
-        )
-    }
-
-    async _processUpdateObjects({next, operation, executeAndLog} : OperationProcessorArgs) {
-        const [collection, where, updates] = operation.slice(1)
-        const affected = await next.process({operation: ['findObjects', collection, where]})
-        const logEntries : ClientSyncLogEntry[] = []
-        for (const object of affected) {
-            const pk = getObjectPk(object, collection, this._storageManager.registry)
-            for (const [fieldName, newValue] of Object.entries(updates)) {
-                logEntries.push({
-                    createdOn: this._getNow(),
-                    syncedOn: null,
-                    field: fieldName,
-                    collection,
-                    operation: 'modify',
-                    pk: pk,
-                    value: newValue
-                })
-            }
-        }
-        
-        await executeAndLog(
-            {placeholder: 'update', operation: 'updateObjects', collection, where, updates},
-            logEntries,
-        )
     }
 }
---
+++ ts/logging-middleware/operation-processors.ts
@@ -0,0 +1,82 @@
+import { getObjectWithoutPk, getObjectPk } from '../utils'
+import { ClientSyncLogEntry } from '../client-sync-log/types'
+import { StorageRegistry } from '@worldbrain/storex';
+
+export type ExecuteAndLog = (originalOperation, logEntries : ClientSyncLogEntry[]) => Promise<any>
+export interface OperationProcessorArgs {
+    next : {process: ({operation}) => any},
+    operation : any[],
+    executeAndLog : ExecuteAndLog,
+    getNow : () => number,
+    storageRegistry : StorageRegistry
+}
+export type OperationProcessor = (args : OperationProcessorArgs) => Promise<any>
+export type OperationProcessorMap = {[operation : string] : OperationProcessor}
+export const DEFAULT_OPERATION_PROCESSORS : OperationProcessorMap = {
+    createObject: _processCreateObject,
+    updateObject: _processUpdateObject,
+    updateObjects: _processUpdateObjects,
+}
+
+async function _processCreateObject({operation, executeAndLog, getNow, storageRegistry} : OperationProcessorArgs) {
+    const [collection, value] = operation.slice(1)
+    const result = await executeAndLog(
+        {placeholder: 'object', operation: 'createObject', collection, args: value},
+        [{
+            collection,
+            createdOn: getNow(),
+            syncedOn: null,
+            operation: 'create',
+            pk: getObjectPk(value, collection, storageRegistry),
+            value: getObjectWithoutPk(value, collection, storageRegistry)
+        } as ClientSyncLogEntry]
+    )
+    const object = result.info.object.object
+    return {object}
+}
+
+async function _processUpdateObject({operation, executeAndLog, getNow, storageRegistry} : OperationProcessorArgs) {
+    const [collection, where, updates] = operation.slice(1)
+    const pk = getObjectPk(where, collection, storageRegistry)
+    const logEntries : ClientSyncLogEntry[] = []
+    for (const [fieldName, newValue] of Object.entries(updates)) {
+        logEntries.push({
+            createdOn: getNow(),
+            syncedOn: null,
+            field: fieldName,
+            collection,
+            operation: 'modify',
+            pk: pk,
+            value: newValue
+        })
+    }
+    await executeAndLog(
+        {placeholder: 'object', operation: 'updateObjects', collection, where, updates},
+        logEntries,
+    )
+}
+
+async function _processUpdateObjects({next, operation, executeAndLog, getNow, storageRegistry} : OperationProcessorArgs) {
+    const [collection, where, updates] = operation.slice(1)
+    const affected = await next.process({operation: ['findObjects', collection, where]})
+    const logEntries : ClientSyncLogEntry[] = []
+    for (const object of affected) {
+        const pk = getObjectPk(object, collection, storageRegistry)
+        for (const [fieldName, newValue] of Object.entries(updates)) {
+            logEntries.push({
+                createdOn: getNow(),
+                syncedOn: null,
+                field: fieldName,
+                collection,
+                operation: 'modify',
+                pk: pk,
+                value: newValue
+            })
+        }
+    }
+    
+    await executeAndLog(
+        {placeholder: 'update', operation: 'updateObjects', collection, where, updates},
+        logEntries,
+    )
+}
