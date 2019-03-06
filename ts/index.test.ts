ae866bbfcf9a348580c179a5aefe1a0bd5e71534
Vincent den Boer <vincent@shishkabab.net>
2019-03-06 15:57:17 +0100

    Integration test receiveLogEntries

--- ts/index.test.ts
+++ ts/index.test.ts
@@ -4,7 +4,9 @@ import { SharedSyncLogStorage } from './shared-sync-log';
 import { ClientSyncLogStorage } from './client-sync-log';
 import { CustomAutoPkMiddleware } from './custom-auto-pk';
 import { SyncLoggingMiddleware } from './logging-middleware';
-import { shareLogEntries } from '.';
+import { shareLogEntries, receiveLogEntries } from '.';
+
+type PromiseContentType<T> = T extends Promise<infer U> ? U : T
 
 describe('Storex sync integration tests', () => {
     async function setupBackend(options) {
@@ -62,49 +64,55 @@ describe('Storex sync integration tests', () => {
             
         }
 
-        return { storageManager, modules, sync, objects: {} }
+        return { storageManager, modules, sync, deviceId: null, objects: {} }
+    }
+
+    async function setupTest(options : {clients? : {name : string}[], getNow : () => number}) {
+        let idsGenerated = 0
+        const pkGenerator = () => `id-${++idsGenerated}`
+
+        const userId = 1
+        const backend = await setupBackend({})
+
+        const clients : {[name : string] : PromiseContentType<ReturnType<typeof setupClient>>} = {}
+        for (const { name } of options.clients || []) {
+            clients[name] = await setupClient({backend, clientName: name, getNow: options.getNow, pkGenerator})
+            clients[name].deviceId = await backend.modules.sharedSyncLog.createDeviceId({ userId, sharedUntil: 10 })
+        }
+
+        return { backend, clients, userId }
     }
 
     describe('shareLogEntries()', () => {
         async function setupShareTest() {
-            let idsGenerated = 0
-            const pkGenerator = () => `id-${++idsGenerated}`
-
-            const backend = await setupBackend({})
-            const client1 = await setupClient({backend, clientName: 'one', getNow: createGetNow({start: 2}), pkGenerator})
-            client1.objects['1'] = (await client1.storageManager.collection('user').createObject({displayName: 'Joe', emails: [{address: 'joe@doe.com'}]})).object
-            
-            const userId = 1
-            const devices = {
-                one: await backend.modules.sharedSyncLog.createDeviceId({ userId, sharedUntil: 10 }),
-                two: await backend.modules.sharedSyncLog.createDeviceId({ userId, sharedUntil: 10 }),
-            }
+            const { backend, clients, userId } = await setupTest({clients: [{ name: 'one' }, { name: 'two' } ], getNow: createGetNow({start: 2})})
+            await clients.one.storageManager.collection('user').createObject({displayName: 'Joe', emails: [{address: 'joe@doe.com'}]})
             
             const share = (options : {now: number}) => shareLogEntries({
-                sharedSyncLog: backend.modules.sharedSyncLog, clientSyncLog: client1.modules.clientSyncLog,
-                userId: 1, deviceId: devices.one, now: options.now
+                sharedSyncLog: backend.modules.sharedSyncLog, clientSyncLog: clients.one.modules.clientSyncLog,
+                userId, deviceId: clients.one.deviceId, now: options.now
             })
 
-            return { backend, share, devices }
+            return { backend, clients, userId, share }
         }
 
         it('should correctly share log entries', async () => {
-            const { backend, share, devices } = await setupShareTest()
+            const { backend, clients, userId, share } = await setupShareTest()
 
             await share({now: 55})
-            expect(await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: devices.two})).toEqual([
+            expect(await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: clients.two.deviceId})).toEqual([
                 {
                     id: expect.anything(),
-                    userId: 1,
-                    deviceId: 1,
+                    userId,
+                    deviceId: clients.one.deviceId,
                     createdOn: 2,
                     sharedOn: 55,
                     data: '{"operation":"create","collection":"user","pk":"id-1","field":null,"value":{"displayName":"Joe"}}',
                 },
                 {
                     id: expect.anything(),
-                    userId: 1,
-                    deviceId: 1,
+                    userId,
+                    deviceId: clients.one.deviceId,
                     createdOn: 3,
                     sharedOn: 55,
                     data: '{"operation":"create","collection":"email","pk":"id-2","field":null,"value":{"address":"joe@doe.com"}}',
@@ -113,18 +121,85 @@ describe('Storex sync integration tests', () => {
         })
 
         it('should not reshare entries that are already shared', async () => {
-            const { backend, share, devices } = await setupShareTest()
+            const { backend, clients, share } = await setupShareTest()
 
             await share({now: 55})
-            const entries = await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: devices.two})
+            const entries = await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: clients.two.deviceId})
             await share({now: 60})
-            expect(await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: devices.two})).toEqual(entries)
+            expect(await backend.modules.sharedSyncLog.getUnsyncedEntries({deviceId: clients.two.deviceId})).toEqual(entries)
         })
     })
 
     describe('receiveLogEntries()', () => {
+        async function setupReceiveTest() {
+            const { backend, clients, userId } = await setupTest({clients: [{ name: 'one' }, { name: 'two' } ], getNow: createGetNow({start: 2})})
+            const receive = async (options : {now : number}) => {
+                await receiveLogEntries({
+                    clientSyncLog: clients.one.modules.clientSyncLog,
+                    sharedSyncLog: backend.modules.sharedSyncLog,
+                    deviceId: clients.one.deviceId,
+                    now: options.now,
+                })
+            }
+            return { backend, clients, userId, receive }
+        }
+
         it('should correctly receive unsynced entries and write them to the local log marked as needing integration', async () => {
+            const { backend, clients, userId, receive } = await setupReceiveTest()
+            await clients.one.storageManager.collection('user').createObject({displayName: 'Bob'})
+
+            await backend.modules.sharedSyncLog.writeEntries([
+                {
+                    userId,
+                    deviceId: clients.one.deviceId,
+                    createdOn: 5,
+                    sharedOn: 55,
+                    data: '{"operation":"create","collection":"user","pk":"id-2","field":null,"value":{"displayName":"Joe"}}',
+                },
+                {
+                    userId,
+                    deviceId: clients.one.deviceId,
+                    createdOn: 7,
+                    sharedOn: 55,
+                    data: '{"operation":"create","collection":"email","pk":"id-3","field":null,"value":{"address":"joe@doe.com"}}',
+                },
+            ], { userId, deviceId: clients.one.deviceId })
             
+            await receive({now: 60})
+            expect(await clients.one.modules.clientSyncLog.getEntriesCreatedAfter(1)).toEqual([
+                {
+                    id: expect.anything(),
+                    createdOn: 2,
+                    sharedOn: null,
+                    needsIntegration: false,
+                    collection: 'user',
+                    pk: 'id-1',
+                    operation: 'create',
+                    value: { displayName: 'Bob' },
+                },
+                {
+                    id: expect.anything(),
+                    createdOn: 5,
+                    sharedOn: 60,
+                    needsIntegration: true,
+                    collection: 'user',
+                    pk: 'id-2',
+                    field: null,
+                    operation: 'create',
+                    value: { displayName: 'Joe' },
+                },
+                {
+                    id: expect.anything(),
+                    createdOn: 7,
+                    sharedOn: 60,
+                    needsIntegration: true,
+                    collection: 'email',
+                    pk: 'id-3',
+                    field: null,
+                    operation: 'create',
+                    value: { address: 'joe@doe.com' },
+                }
+            ])
         })
     })
 
