40af516927f3d65712744dc069b1465748b0d1e6
Vincent den Boer <vincent@shishkabab.net>
2019-10-29 13:30:39 +0100

    Add high-level integration modules

---
+++ ts/integration/continuous-sync.ts
@@ -0,0 +1,141 @@
+import { Browser } from 'webextension-polyfill-ts'
+import StorageManager from '@worldbrain/storex'
+import { SharedSyncLog } from '@worldbrain/storex-sync/lib/shared-sync-log'
+import { reconcileSyncLog } from '@worldbrain/storex-sync/lib/reconciliation'
+import {
+    doSync,
+    SyncPreSendProcessor,
+    SyncSerializer,
+} from '@worldbrain/storex-sync'
+import { ClientSyncLogStorage } from '@worldbrain/storex-sync/lib/client-sync-log'
+import { RecurringTask } from '../utils/recurring-task'
+
+export interface ContinuousSyncSettingsStore {
+    retrieveSetting(
+        key: ContinuousSyncSetting
+    ): Promise<ContinuousSyncSettingValue>
+    storeSetting(
+        key: ContinuousSyncSetting,
+        value: ContinuousSyncSettingValue,
+    ): Promise<void>
+}
+export type ContinuousSyncSetting = 'continuousSyncEnabled' | 'deviceId'
+export type ContinuousSyncSettingValue = boolean | number | string | null
+
+export interface ContinuousSyncDependencies {
+    auth: { getUserId(): Promise<number | string | null> }
+    storageManager: StorageManager
+    clientSyncLog: ClientSyncLogStorage
+    getSharedSyncLog: () => Promise<SharedSyncLog>
+    settingStore: ContinuousSyncSettingsStore
+    frequencyInMs?: number
+    toggleSyncLogging: (enabled: boolean) => void
+}
+export class ContinuousSync {
+    public recurringIncrementalSyncTask?: RecurringTask
+    public deviceId?: number | string
+    public enabled = false
+    public useEncryption = true
+
+    constructor(
+        private dependencies: ContinuousSyncDependencies,
+    ) {
+    }
+
+    async setup() {
+        const enabled = await this.dependencies.settingStore.retrieveSetting('continuousSyncEnabled')
+        if (!enabled) {
+            return
+        }
+
+        this.deviceId = (await this.dependencies.settingStore.retrieveSetting('deviceId')) as string | number
+        this.setupContinuousSync()
+    }
+
+    async tearDown() {
+        if (this.recurringIncrementalSyncTask) {
+            this.recurringIncrementalSyncTask.stop()
+        }
+    }
+
+    setupRecurringTask() {
+        if (this.dependencies.frequencyInMs) {
+            this.recurringIncrementalSyncTask = new RecurringTask(
+                () => this.maybeDoIncrementalSync(),
+                {
+                    intervalInMs: this.dependencies.frequencyInMs,
+                    onError: () => { },
+                },
+            )
+        }
+    }
+
+    async initDevice() {
+        const existingDeviceId = await this.dependencies.settingStore.retrieveSetting('deviceId')
+        if (existingDeviceId) {
+            return
+        }
+
+        const sharedSyncLog = await this.dependencies.getSharedSyncLog()
+        const newDeviceId = await sharedSyncLog.createDeviceId({
+            userId: await this.dependencies.auth.getUserId(),
+            sharedUntil: 1,
+        })
+        await this.dependencies.settingStore.storeSetting('deviceId', newDeviceId)
+        this.deviceId = newDeviceId
+    }
+
+    async enableContinuousSync() {
+        await this.dependencies.settingStore.storeSetting('continuousSyncEnabled', true)
+        await this.setupContinuousSync()
+    }
+
+    async setupContinuousSync() {
+        this.dependencies.toggleSyncLogging(true)
+        this.enabled = true
+        this.setupRecurringTask()
+    }
+
+    async forceIncrementalSync() {
+        if (this.enabled) {
+            if (this.recurringIncrementalSyncTask) {
+                await this.recurringIncrementalSyncTask.forceRun()
+            } else {
+                this.doIncrementalSync()
+            }
+        }
+    }
+
+    async maybeDoIncrementalSync() {
+        if (this.enabled) {
+            await this.doIncrementalSync()
+        }
+    }
+
+    private async doIncrementalSync() {
+        const { auth } = this.dependencies
+        const userId = await auth.getUserId()
+        if (!userId) {
+            throw new Error(`Cannot Sync without authenticated user`)
+        }
+        await doSync({
+            clientSyncLog: this.dependencies.clientSyncLog,
+            sharedSyncLog: await this.dependencies.getSharedSyncLog(),
+            storageManager: this.dependencies.storageManager,
+            reconciler: reconcileSyncLog,
+            now: Date.now(),
+            userId,
+            deviceId: this.deviceId,
+            serializer: this.getSerializer() || undefined,
+            preSend: this.getPreSendProcessor() || undefined,
+        })
+    }
+
+    getPreSendProcessor(): SyncPreSendProcessor | void {
+
+    }
+
+    getSerializer(): SyncSerializer | void {
+
+    }
+}
---
+++ ts/integration/initial-sync.ts
@@ -0,0 +1,217 @@
+import pick from 'lodash/pick'
+import Peer from 'simple-peer'
+import { SignalTransport, SignalChannel } from 'simple-signalling/lib/types'
+import {
+    signalSimplePeer,
+    SimplePeerSignallingEvents,
+} from 'simple-signalling/lib/simple-peer'
+import {
+    FastSyncSender,
+    FastSyncReceiver,
+    FastSyncEvents,
+    FastSyncPreSendProcessor,
+} from '@worldbrain/storex-sync/lib/fast-sync'
+import {
+    WebRTCFastSyncSenderChannel,
+    WebRTCFastSyncReceiverChannel,
+} from '@worldbrain/storex-sync/lib/fast-sync/channels'
+import TypedEmitter from 'typed-emitter'
+import StorageManager from '@worldbrain/storex'
+import {
+    FastSyncSenderChannel,
+    FastSyncReceiverChannel,
+} from '@worldbrain/storex-sync/lib/fast-sync/types'
+
+export type InitialSyncInfo = {
+    signalChannel: SignalChannel
+    events: TypedEmitter<InitialSyncEvents>
+    finishPromise: Promise<void>
+} & (
+        | {
+            role: 'sender'
+            senderFastSyncChannel: FastSyncSenderChannel
+            senderFastSync: FastSyncSender
+        }
+        | {
+            role: 'receiver'
+            receiverFastSyncChannel: FastSyncReceiverChannel
+            receiverFastSync: FastSyncReceiver
+        })
+
+export type InitialSyncEvents = FastSyncEvents &
+    SimplePeerSignallingEvents & {
+        connecting: {}
+        releasingSignalChannel: {}
+        connected: {}
+        finished: {}
+    }
+
+export interface InitialSyncDependencies {
+    storageManager: StorageManager
+    signalTransportFactory: SignalTransportFactory
+    syncedCollections: string[]
+}
+
+export type SignalTransportFactory = () => SignalTransport
+export class InitialSync {
+    public wrtc: any // Possibility for tests to inject wrtc library
+    private initialSyncInfo?: InitialSyncInfo
+
+    constructor(
+        protected dependencies: InitialSyncDependencies,
+    ) { }
+
+    async requestInitialSync(options?: {
+        excludePassiveData?: boolean
+    }): Promise<{ initialMessage: string }> {
+        const role = 'sender'
+        const {
+            signalTransport,
+            initialMessage,
+        } = await this._createSignalTransport(role)
+        this.initialSyncInfo = await this._setupInitialSync({
+            role,
+            signalTransport,
+            initialMessage,
+            deviceId: 'first',
+            ...(options || {}),
+        })
+
+        return { initialMessage }
+    }
+
+    async answerInitialSync(options: {
+        initialMessage: string
+    }): Promise<void> {
+        const role = 'receiver'
+        const { signalTransport } = await this._createSignalTransport(role)
+        this.initialSyncInfo = await this._setupInitialSync({
+            role,
+            signalTransport,
+            initialMessage: options.initialMessage,
+            deviceId: 'second',
+        })
+    }
+
+    async waitForInitialSync(): Promise<void> {
+        if (this.initialSyncInfo) {
+            await this.initialSyncInfo.finishPromise
+        }
+    }
+
+    _createSignalTransport(
+        role: 'sender',
+    ): Promise<{ signalTransport: SignalTransport; initialMessage: string }>
+    _createSignalTransport(
+        role: 'receiver',
+    ): Promise<{ signalTransport: SignalTransport }>
+    async _createSignalTransport(
+        role: 'sender' | 'receiver',
+    ): Promise<{
+        signalTransport: SignalTransport
+        initialMessage: string | undefined
+    }> {
+        const signalTransport: SignalTransport = this.dependencies.signalTransportFactory()
+        return {
+            signalTransport,
+            initialMessage:
+                role === 'sender'
+                    ? (await signalTransport.allocateChannel()).initialMessage
+                    : undefined,
+        }
+    }
+
+    async _setupInitialSync(options: {
+        role: 'sender' | 'receiver'
+        signalTransport: SignalTransport
+        initialMessage: string
+        deviceId: 'first' | 'second'
+    }): Promise<InitialSyncInfo> {
+        const signalChannel = await options.signalTransport.openChannel(
+            pick(options, 'initialMessage', 'deviceId'),
+        )
+        const peer = new Peer({
+            initiator: options.role === 'receiver',
+            wrtc: this.wrtc,
+        })
+
+        let senderFastSyncChannel: FastSyncSenderChannel | undefined
+        let receiverFastSyncChannel: FastSyncReceiverChannel | undefined
+        let senderFastSync: FastSyncSender | undefined
+        let receiverFastSync: FastSyncReceiver | undefined
+        let fastSync: {
+            execute: () => Promise<void>
+            events: TypedEmitter<FastSyncEvents & InitialSyncEvents>
+        }
+
+        if (options.role === 'sender') {
+            senderFastSyncChannel = new WebRTCFastSyncSenderChannel({ peer })
+            senderFastSync = new FastSyncSender({
+                storageManager: this.dependencies.storageManager,
+                channel: senderFastSyncChannel,
+                collections: this.dependencies.syncedCollections,
+                preSendProcessor: this.getPreSendProcessor() || undefined,
+            })
+            fastSync = senderFastSync
+        } else {
+            receiverFastSyncChannel = new WebRTCFastSyncReceiverChannel({
+                peer,
+            })
+            receiverFastSync = new FastSyncReceiver({
+                storageManager: this.dependencies.storageManager,
+                channel: receiverFastSyncChannel,
+            })
+            fastSync = receiverFastSync
+        }
+
+        const buildInfo = (): InitialSyncInfo => {
+            const common = {
+                signalChannel,
+                finishPromise,
+                events: fastSync.events,
+            }
+            if (options.role === 'sender') {
+                return {
+                    role: 'sender',
+                    ...common,
+                    senderFastSync,
+                    senderFastSyncChannel,
+                }
+            } else {
+                return {
+                    role: 'receiver',
+                    ...common,
+                    receiverFastSync,
+                    receiverFastSyncChannel,
+                }
+            }
+        }
+
+        const finishPromise: Promise<void> = (async () => {
+            fastSync.events.emit('connecting', {})
+            await signalChannel.connect()
+            await signalSimplePeer({
+                signalChannel,
+                simplePeer: peer,
+                reporter: (eventName, event) =>
+                    (fastSync.events as any).emit(eventName, event),
+            })
+            fastSync.events.emit('releasingSignalChannel', {})
+            await signalChannel.release()
+            fastSync.events.emit('connected', {})
+
+            await this.preSync(buildInfo())
+            await fastSync.execute()
+            fastSync.events.emit('finished', {})
+        })()
+
+        return buildInfo()
+    }
+
+    protected getPreSendProcessor(): FastSyncPreSendProcessor | void {
+
+    }
+
+    protected async preSync(options: InitialSyncInfo) {
+    }
+}
