471d82e75f9e80cf1deca86598ea34ef7778090e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-07-16 10:46:49 +0700

    Add indices on `sharedOn` + `needsIntegration` in `clientSyncLogEntry`

--- ts/client-sync-log/index.ts
+++ ts/client-sync-log/index.ts
@@ -12,12 +12,12 @@ export class ClientSyncLogStorage extends StorageModule {
         return {
             collections: {
                 clientSyncLogEntry: {
-                    version: new Date('2019-02-05'),
+                    version: new Date('2020-07-16'),
                     fields: {
                         createdOn: { type: 'timestamp' },
-                        sharedOn: { type: 'timestamp', optional: true }, // when was this sent or received?
+                        sharedOn: { type: 'timestamp' }, // when was this sent or received?
                         deviceId: { type: 'json' }, // what device did this operation happen on?
-                        needsIntegration: { type: 'boolean', optional: true },
+                        needsIntegration: { type: 'int' },
                         collection: { type: 'string' },
                         pk: { type: 'json' },
                         field: { type: 'string', optional: true },
@@ -28,6 +28,32 @@ export class ClientSyncLogStorage extends StorageModule {
                         { field: ['deviceId', 'createdOn'], pk: true },
                         { field: 'createdOn' },
                         { field: ['collection', 'pk'] },
+                        { field: 'sharedOn' },
+                        { field: 'needsIntegration' },
+                    ],
+                    history: [
+                        {
+                            version: new Date('2019-02-05'),
+                            fields: {
+                                createdOn: { type: 'timestamp' },
+                                sharedOn: { type: 'timestamp', optional: true }, // when was this sent or received?
+                                deviceId: { type: 'json' }, // what device did this operation happen on?
+                                needsIntegration: {
+                                    type: 'boolean',
+                                    optional: true,
+                                },
+                                collection: { type: 'string' },
+                                pk: { type: 'json' },
+                                field: { type: 'string', optional: true },
+                                operation: { type: 'string' },
+                                value: { type: 'json', optional: true },
+                            },
+                            indices: [
+                                { field: ['deviceId', 'createdOn'], pk: true },
+                                { field: 'createdOn' },
+                                { field: ['collection', 'pk'] },
+                            ],
+                        },
                     ],
                 },
                 // clientSyncLogInfo: {
